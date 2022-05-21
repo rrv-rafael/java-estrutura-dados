c560914dd711d436a7821422e8386ef093d358ea
Rafael Rodrigues <rrv.rafael@outlook.com>
2022-05-20 22:38:49 -0300

    atualizações

--- src/App.java
+++ src/App.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
+import br.com.rrvrafael.modelos.Pessoa;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        int opcao;
+
+        System.out.println("*****Menu*****");
+        System.out.println("1 - Gerenciamento de memória.");
+        System.out.println("Conforme as opções cima, informe a desejada:");
+        opcao = scan.nextInt();
+
+        switch (opcao) {
+            case 1 -> gerenciamentoMemoria();
+            default -> System.out.println("Opção inválida!");
+        }
+
+        scan.close();
+    }
+
+    public static void gerenciamentoMemoria() {
+        int valor = 3;
+        System.out.println("valor: " + valor);
+
+        int valor2 = valor;
+        System.out.println("valor2: " + valor2);
+        valor2 = 2;
+
+        System.out.println("valor: " + valor);
+        System.out.println("valor2: " + valor2);
+        System.out.println(valor == valor2);
+
+        Pessoa p1 = new Pessoa(1, "Rafael Rodrigues");
+
+        Pessoa p2 = p1;
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        p2.setNome("Rafael Modificado");
+
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
---
+++ src/br/com/rrvrafael/modelos/Pessoa.java
@@ -0,0 +1,31 @@
+package br.com.rrvrafael.modelos;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados da Pessoa:\nId: " + id + "\nNome: " + nome;
+    }
+
+    
+}
