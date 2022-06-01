fd465b96c3320d5d16ed6485e2bdc335e356926d
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2022-05-31 22:39:36 -0300

    implementando lista ligada

--- src/br/com/rrvrafael/listasligadas/ListaLigada.java
+++ src/br/com/rrvrafael/listasligadas/ListaLigada.java
@@ -18,12 +18,43 @@ public class ListaLigada<T> {
             primeiroNo = novoNo;
             ultimoNo = novoNo;
         } else {
-            primeiroNo = ultimoNo;
+            ultimoNo.setProximo(novoNo);
             ultimoNo = novoNo;
         }
+
+        tamanho++;
     }
 
     public boolean estaVazia() {
         return tamanho == 0;
+    }
+
+    public int tamanho() {
+        return tamanho;
+    }
+
+    @Override
+    public String toString() {
+        if (estaVazia()) {
+            return "Lista []";
+        } else {
+            No<T> noAtual = primeiroNo;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("Lista [");
+            sb.append(noAtual.getElemento() != null ? noAtual.getElemento().toString() : "<NULO>");
+            sb.append(",");
+
+            while (noAtual.getProximo() != null) {
+                sb.append(noAtual.getProximo().getElemento() != null ? noAtual.getProximo().getElemento().toString() : "<NULO>");
+                sb.append(",");
+                noAtual = noAtual.getProximo();
+            }
+
+            sb.append("]");
+
+            return sb.toString();
+        }
     }
 }
